cf9278c1caa523f8f618fdbf9eb7a1033cc6089f
NevenaD <nevena.dresevic17@gmail.com>
2017-11-04 22:38:20 +0100

    dva dugmeta

--- resources/views/company/config.blade.php
+++ resources/views/company/config.blade.php
@@ -1,2 +1,117 @@
-company
-config
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Edit Company Profile</h1>
+        <br>
+        <h2>Personal information</h2>
+        <table class="table">
+            <tr>
+                <td>Full company name</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Company moto</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Address</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Phone</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+        </table>
+
+        <h2>Workplace</h2>
+        <table class="table">
+            <tr>
+                <td>Location description</td>
+                <td><input type="text-area" class="form-control"></td>
+            </tr>
+            <br>
+            <tr>
+                <td>Pictures</td>
+            </tr>
+            <tr>
+                <td><button type="button" name="kreiraj" class="form-control">Upload picture</td>
+            </tr>
+            <tr>
+                <img src="" alt="Image" width="139" height="150" class="form-control">
+            </tr>
+
+                <!-- <br>
+                <td><img src="" alt="First picture" width="139" height="150" style="margin: 0 10px 10px 0;" />
+                <button type="button" name="kreiraj" class="form-control">Upload picture
+                </td>
+                <td>
+                    <div class="btn-pref btn-group btn-group-justified btn-group-lg" role="group" aria-label="...">
+                        <div class="btn-group" role="group">
+                            <button type="button" class="btn btn-default" href="#tab1" data-toggle="tab">
+                                <div class="hidden-xs">Upload picture 1</div>
+                            </button>
+                        </div>
+                        <div class="btn-group" role="group">
+                            <button type="button" class="btn btn-default" href="#tab2" data-toggle="tab">
+                                <div class="hidden-xs">Upload picture 2</div>
+                            </button>
+                        </div>
+                        <div class="btn-group" role="group">
+                            <button type="button" class="btn btn-default" href="#tab3" data-toggle="tab">
+                                <div class="hidden-xs">Upload picture 3</div>
+                            </button>
+                        </div>
+                </td>
+            </tr> -->
+
+        </table>
+
+        <h2>Jobs</h2>
+        <h4>New job</h4>
+        <table class="table">
+            <tr>
+                <td>Name</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Category</td>
+                <td><select name="cars" class="form-control">
+                        <option value="volvo">Kategorija 1</option>
+                        <option value="saab">Kategorija 2</option>
+                        <option value="fiat">Kategorija 3</option>
+                        <option value="audi">Kategorija 4</option>
+                    </select></td>
+            </tr>
+            <tr>
+                <td>Description</td>
+                <td><input type="text-area" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Requirements</td>
+                <td><input type="text-area" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Optional qualities</td>
+                <td><input type="text-area" class="form-control"></td>
+            </tr>
+            <tr>
+                <td><button type="button" name="kreiraj" class="form-control">Create job</td>
+            </tr>
+        </table>
+
+        <table class="table">
+            <tr>
+                <td><button type="button" name="kreiraj" class="form-control">Save</td>
+            </tr>
+        </table>
+
+
+
+
+
+@endsection
--- resources/views/company/new-job.blade.php
+++ resources/views/company/new-job.blade.php
@@ -1,5 +1,80 @@
 @extends('layouts.app')
 
 @section('content')
-    new job
+    <div class="container">
+        <h1>New Job</h1>
+        <br>
+        <h2>Informations</h2>
+        <table class="table">
+            <tr>
+                <td>Required position</td>
+                <td><select name="positions" class="form-control">
+                        <option value="" disabled selected>Choose position..</option>
+                        <option value="junior">Junior Developer</option>
+                        <option value="senior">Senior Developer</option>
+                    </select></td>
+            </tr>
+            <tr>
+                <td>Category</td>
+                <td><select name="jobs" class="form-control">
+                        <option value="" disabled selected>Choose category..</option>
+                        <option value="cs">Computer Science</option>
+                        <option value="ee">Electrical Engineering</option>
+                        <option value="m">Mathematics</option>
+                        <option value="se">Software Engineering</option>
+                        <option value="wb">Web Development</option>
+                    </select></td>
+            </tr>
+            <tr>
+                <td>What kind of project?</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Job description</td>
+                <td><input type="text-area" style="height: 150px;" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Determine minimum grade for:</td>
+                <table class="table">
+                    <tr>
+                        <div class="panel panel-default" style="width: 115px; border: hidden" >
+                            <div class="panel-body">
+                                C<input type="text-area" style="width: 70px;"class="form-control"></td>
+                                <br>
+                                C++<input type="text-area" style="width: 70px;"class="form-control"></td>
+                                <br>
+                                Java<input type="text-area" style="width: 70px;"class="form-control"></td>
+                                <br>
+                                JavaScript<input type="text-area" style="width: 70px;"class="form-control"></td>
+                                <br>
+                                HTML<input type="text-area" style="width: 70px;"class="form-control"></td>
+                                <br>
+                                Python<input type="text-area" style="width: 70px;"class="form-control"></td>
+                            </div>
+                        </div>
+                        <!--<td> <button type="button" class="btn btn-default" style="width: 125px;">Make test</button>
+                        </td>
+                        <td>
+                            <button type="button" class="btn btn-success" style="width: 125px;">Create job</button></td>
+                        </td>-->
+                    </tr>
+                </table>
+                <br>
+            </tr>
+        </table>
+        <table class="table">
+            <tr>
+                <td>
+                    <button type="button" class="btn btn-default" style="width: 125px;">Make test</button>
+                </td>
+            </tr>
+
+            <table class="table">
+                <tr>
+                    <td>
+                        <button type="button" class="btn btn-success" style="width: 125px;">Create job</button>
+                    </td>
+                </tr>
+            </table>
+    </div>
 @endsection
