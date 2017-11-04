f24d86357ac2108b94ec2a131c27ee0854f0eb1c
NevenaD <nevena.dresevic17@gmail.com>
2017-11-04 17:08:51 +0100

    dva dugmeta

--- resources/views/auth/register-as.blade.php
+++ resources/views/auth/register-as.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="{{url('register-company')}}">As company</a>
-    <a href="{{url('register-developer')}}">As developer</a>
+    <div class="container">
+        <div class="text-center">
+            <a class="btn btn-default btn-lg" style="color:#ff1d0e; width: 200px; position: relative;
+                    " href="{{url('register-company')}}">As company</a>
+            <br>
+            <br>
+            <a class="btn btn-default btn-lg" style="color:DodgerBlue; width: 200px; position: relative;" href="{{url('register-developer')}}">As developer</a>
+
+        </div>
+    </div>
+
+
 @endsection
